df0209024b6624161bbce94c706ca588413593fe
Space Walker <spacedoesrs@gmail.com>
2023-01-23 22:44:07 +0100

    fix chiseled bookshelf

---
+++ src/main/java/signal/impl/mixin/common/block/ChiseledBookShelfBlockMixin.java
@@ -0,0 +1,30 @@
+package signal.impl.mixin.common.block;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.ChiseledBookShelfBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.ChiseledBookShelfBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+import signal.api.signal.block.AnalogSignalSource;
+
+@Mixin(ChiseledBookShelfBlock.class)
+public class ChiseledBookShelfBlockMixin implements AnalogSignalSource {
+
+	@Override
+	public int getAnalogSignal(Level level, BlockPos pos, BlockState state, int min, int max) {
+		if (!level.isClientSide()) {
+			BlockEntity blockEntity = level.getBlockEntity(pos);
+
+			if (blockEntity instanceof ChiseledBookShelfBlockEntity) {
+				ChiseledBookShelfBlockEntity bookShelf = (ChiseledBookShelfBlockEntity) blockEntity;
+				return AnalogSignalSource.getAnalogSignal(bookShelf.getLastInteractedSlot() + 1, min, max);
+			}
+		}
+
+		return min;
+	}
+}
